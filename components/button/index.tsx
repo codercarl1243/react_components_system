675ad9458b6754abe9385324f7a0905084e6fb7a
codercarl1243 <codercarl1243@gmail.com>
2025-08-19 16:56:19 +1000

    ADD Button component

---
+++ components/button/index.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import clsx from "clsx";
+import { BaseButtonProps } from "./button.type"
+import useButton from "./useButton"
+
+
+export default function Button({
+    className,
+    children,
+    onClick,
+    type = "button",
+    disabled,
+    isLoading = false,
+    ref,
+    ...props }: BaseButtonProps) {
+
+    const { handleClick } = useButton();
+
+    return (
+        <button
+            className={clsx(className, "button")}
+            onClick={handleClick(onClick)}
+            disabled={isLoading || disabled}
+            ref={ref}
+            type={type}
+            aria-busy={isLoading}
+            {...props}
+            data-testid="base-button"
+        >
+            {isLoading ? "Loading" : children}
+        </button>
+    )
+}
