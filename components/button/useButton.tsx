1f16c83f5a5666c02d675c1dcb14af499faa37a1
codercarl1243 <codercarl1243@gmail.com>
2025-08-19 16:56:36 +1000

    Add Button Hook

---
+++ components/button/useButton.tsx
@@ -0,0 +1,22 @@
+
+type MouseEventType = React.MouseEvent<HTMLButtonElement>;
+
+type ButtonClickHandler<T = void> = (event: MouseEventType) => T | Promise<T>;
+
+export default function useButton() {
+
+    const handleClick = <T = void>(userHandler?: ButtonClickHandler<T>) =>
+        async (event: MouseEventType) => {
+
+            if (!userHandler) return;
+            try {
+                return await userHandler(event);
+            } catch (err) {
+                console.error("Button click error", err);
+                throw err;
+            }
+
+        }
+
+    return { handleClick }
+}
