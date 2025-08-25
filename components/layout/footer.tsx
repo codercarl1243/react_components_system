0cb975a27827f98c60a3f7b7701cd047484b7a55
codercarl1243 <codercarl1243@gmail.com>
2025-08-25 11:42:22 +1000

    UPDATE footer to include current year in copyright

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,10 @@
 
 
 export default function Footer(){
-
+const year = new Date().getFullYear();
     return (
         <footer>
-            <p>&copy; 2025 Carl Davidson. All rights reserved. | <a className="link" href="https://codercarl.dev">codercarl.dev</a></p>
+            <p>&copy; {year} Carl Davidson. All rights reserved. | <a className="link" href="https://codercarl.dev">codercarl.dev</a></p>
         </footer>
     )
 }
