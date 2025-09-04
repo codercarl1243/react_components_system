b8b75119a039470ec862de152dc8f8eff4b7056a
codercarl1243 <codercarl1243@gmail.com>
2025-09-04 10:39:27 +1000

    UPDATE header file to export Header and not improperly named Navigation

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,7 +1,7 @@
 import SkipLink from "@/components/skiplink";
 import type { ComponentProps } from "react";
 
-export default function Navigation(props: ComponentProps<'header'>) {
+export default function Header(props: ComponentProps<'header'>) {
 
     return (
         <header className="header" aria-label="Main" {...props}>
