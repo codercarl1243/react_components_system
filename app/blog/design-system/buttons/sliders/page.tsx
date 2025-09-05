776d0da7da77ef931bff4f70f7e8abbf9843b123
codercarl1243 <codercarl1243@gmail.com>
2025-09-05 16:33:19 +1000

    ADD metadata should be on each page

--- app/blog/design-system/buttons/sliders/page.tsx
+++ app/blog/design-system/buttons/sliders/page.tsx
@@ -4,6 +4,9 @@ import PostBanner from "@/components/post/post.banner";
 import PostSection from "@/components/post/post.section";
 import PostSideBar from "@/components/post/post.sidebar";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = { title: "Buttons · Design System" };
 
 export default function SlidersPage(){
 
