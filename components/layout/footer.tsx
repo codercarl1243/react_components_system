f16b170d02de2aca5a513d26e3594f03b1309fd3
codercarl1243 <codercarl1243@gmail.com>
2025-09-05 11:18:30 +1000

    UPDATE footer styles to ensure styling is consistently targeting classnames

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,7 +4,7 @@ import type { ComponentProps } from "react";
 export default function Footer(props: ComponentProps<'footer'>){
 const year = new Date().getFullYear();
     return (
-        <footer {...props}>
+        <footer className="footer" {...props}>
             <p>&copy; {year} Carl Davidson. All rights reserved. | <a className="link" href="https://codercarl.dev">codercarl.dev</a></p>
         </footer>
     )
