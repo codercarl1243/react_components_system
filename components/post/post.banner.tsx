006706d87de4860d891396853c25c8fd196a809d
codercarl1243 <codercarl1243@gmail.com>
2025-09-05 13:04:31 +1000

    UPDATE postbanner should pop on the page

--- components/post/post.banner.tsx
+++ components/post/post.banner.tsx
@@ -3,16 +3,17 @@ import type { PostBannerPropsType } from "@/components/post/post.type";
 import Heading from "@/components/heading";
 
 
-export default function PostBanner({ title, image }: PostBannerPropsType) {
+export default function PostBanner({ title, subtitle, image }: PostBannerPropsType) {
 
     return (
-        <div className="post__banner">
+        <div className="post__banner font-accent ">
 
             {image?.src ? (
                 <Image src={image.src} alt={image.alt ?? ""} height={400} width={1200} />
             )
                 : null}
             <Heading headingLevel={1}>{title}</Heading>
+            {subtitle && <p className="italic text-lg">{subtitle}</p>}
         </div>
     )
 }
