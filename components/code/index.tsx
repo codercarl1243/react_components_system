0465d20e9eee1315d548dc597371c377f896edae
codercarl1243 <codercarl1243@gmail.com>
2025-09-08 11:38:55 +1000

    UPDATE Code component shouldnt be copying rendered html it should be rendering the codeString

--- components/code/index.tsx
+++ components/code/index.tsx
@@ -40,7 +40,7 @@ export default async function Code({
     if (!inline) {
         return <div
             className={`shiki-wrapper width-${layout}`}>
-            {copyEnabled && <CopyButton text={out}/>}
+            {copyEnabled && <CopyButton text={codeString}/>}
             <div {...(title && { 'aria-label': title, role: "region" })}
                 dangerouslySetInnerHTML={{ __html: out }} />
         </div>
