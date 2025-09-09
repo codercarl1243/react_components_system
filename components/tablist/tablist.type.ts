824965312446f8dbf20f51a37c3ece925080b333
codercarl1243 <codercarl1243@gmail.com>
2025-09-09 17:24:30 +1000

    UPDATE types should be readonly to discourage mutations

--- components/tablist/tablist.type.ts
+++ components/tablist/tablist.type.ts
@@ -1,28 +1,28 @@
 import type { ReactNode, HTMLAttributes, ComponentPropsWithRef } from "react";
 import { BaseButtonProps } from "../button/button.type";
 
-export interface TabProps extends Omit<BaseButtonProps,
-  'id' | 'role' | 'tabIndex' | 'aria-selected' | 'aria-controls'
-> {
+export type TabProps = Readonly<{
   id: string;
   isSelected: boolean;
-}
+} & Omit<BaseButtonProps,
+  'id' | 'role' | 'tabIndex' | 'aria-selected' | 'aria-controls'
+>>
 
-export interface TabPanelProps extends Omit<ComponentPropsWithRef<'div'>,
-  'id' | 'role' | 'tabIndex' | 'aria-labelledby'
-> {
+export type TabPanelProps = Readonly<{
   id: string;
-}
+} & Omit<ComponentPropsWithRef<'div'>,
+  'id' | 'role' | 'tabIndex' | 'aria-labelledby'
+>>
 
-export type TabItem = {
+export type TabItem = Readonly<{
   id: string;
   panelContent: ReactNode;
   tabLabel: ReactNode;
-}
+}>
 
-export interface TabListProps extends HTMLAttributes<HTMLDivElement> {
+export type TabListProps = Readonly<{
   tabs: TabItem[];
   orientation?: 'vertical' | 'horizontal';
   defaultActiveTabId?: string;
   // TODO: figure out what I want to do re: heading for the tablist which can be used as an aria label or a Heading element
-}
+} & HTMLAttributes<HTMLDivElement>>;
