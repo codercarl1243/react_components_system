bd6369735a7589fc291ea8e2db2afe022b53743c
codercarl1243 <codercarl1243@gmail.com>
2025-09-12 14:41:16 +1000

    UPDATE usebutton hook shouldnt be noisy in prod

--- components/button/useButton.ts
+++ components/button/useButton.ts
@@ -4,14 +4,18 @@ type MouseEventType = MouseEvent<HTMLButtonElement>;
 
 type ButtonClickHandler<T = void> = (event: MouseEventType) => T | Promise<T>;
 
-export default function useButton () {
+export default function useButton() {
   const handleClick = <T = void>(userHandler?: ButtonClickHandler<T>) =>
     async (event: MouseEventType) => {
       if (!userHandler) return
       try {
         return await userHandler(event)
       } catch (err: unknown) {
-        console.error('Button click error', err)
+        if (process.env.NODE_ENV !== 'production') {
+          // eslint-disable-next-line no-console
+          console.error('Button click error', err)
+
+        }
         throw err
       }
     }
