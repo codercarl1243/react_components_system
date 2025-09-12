2fdb6eaf73a735a8070c584128dbc4296b6bc93a
codercarl1243 <codercarl1243@gmail.com>
2025-09-12 13:14:44 +1000

    UPDATE linter should be on v9+ if possible

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,26 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import next from '@next/eslint-plugin-next'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-})
-
-const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'standard'
-  ),
+export default [
+  js.configs.recommended,
+  next.configs['core-web-vitals'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      // Add TypeScript rules here
+    },
+  },
   {
     ignores: [
       'node_modules/**',
@@ -25,5 +31,3 @@ const eslintConfig = [
     ]
   }
 ]
-
-export default eslintConfig
