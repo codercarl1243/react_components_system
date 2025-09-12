0fbd0275dfee4015d8361b55b8380091d5b0b8b7
codercarl1243 <codercarl1243@gmail.com>
2025-09-12 13:22:39 +1000

    UPDATE ensure react fragments and custom components dont bypass id check

--- components/post/post.note.tsx
+++ components/post/post.note.tsx
@@ -5,65 +5,51 @@ import clsx from 'clsx'
 
 type ElementWithId = ReactElement<{ id?: string }>;
 
-function isDomElement (element: unknown): element is ElementWithId {
+function isDomElement(element: unknown): element is ElementWithId {
   return (
     isValidElement(element) &&
-        typeof element === 'object' &&
-        element !== null &&
-        'props' in element &&
-        typeof element.props === 'object' &&
-        element.props !== null
+    typeof element === 'object' &&
+    element !== null &&
+    'props' in element &&
+    typeof element.props === 'object' &&
+    element.props !== null
   )
 }
 
-function isElementWithIdProps (element: unknown): element is ElementWithId {
-  if (!isDomElement(element)) return false
-
-  const elementType = element.type
-  if (typeof elementType === 'string') return true
-
-  return true
-}
-
-export default function PostNote ({ className, children, ...props }: ComponentProps<'div'>) {
+export default function PostNote({ className, children, ...props }: ComponentProps<'div'>) {
   const id = useId()
-
   const childArray = Children.toArray(children)
-
   if (childArray.length === 0) return null
 
   const [firstChild, ...restChildren] = childArray
   const isFirstChildDomElement = isDomElement(firstChild)
-  const canAcceptId = isElementWithIdProps(firstChild)
-
-  const getElementId = (element: ElementWithId): string | undefined => {
-    return element.props?.id
-  }
-
+  const getElementId = (element: ElementWithId): string | undefined => element.props?.id
   const existingId = isFirstChildDomElement ? getElementId(firstChild) : undefined
   const labelId = existingId || id
 
-  const firstChildWithId = canAcceptId
-    ? cloneElement(firstChild, { id: labelId })
-    : <span id={labelId}>{firstChild}</span>
+  // Only assign id to DOM elements, otherwise wrap in span
+  const firstChildWithId =
+    isFirstChildDomElement && typeof firstChild.type === 'string'
+      ? cloneElement(firstChild, { id: labelId })
+      : <span id={labelId}>{firstChild}</span>
 
   // TODO: Consider making the content expandable and hidden
   return (
-        <div
-            // data-expanded={"false"}
-            className={clsx(className, 'post-note width-bleed')}
-            role={'note'}
-            aria-labelledby={labelId}
-            {...props}>
-            <div className="post-note__first">
-                <Icon icon={RiInformationLine} size={64} className="post-note__icon" />
-                {firstChildWithId}
-            </div>
-            {restChildren.length > 0 &&
-                <div className="flow-4 post-note__content">
-                    {restChildren}
-                </div>
-            }
+    <div
+      // data-expanded={"false"}
+      className={clsx(className, 'post-note width-bleed')}
+      role={'note'}
+      aria-labelledby={labelId}
+      {...props}>
+      <div className="post-note__first">
+        <Icon icon={RiInformationLine} size={64} className="post-note__icon" />
+        {firstChildWithId}
+      </div>
+      {restChildren.length > 0 &&
+        <div className="flow-4 post-note__content">
+          {restChildren}
         </div>
+      }
+    </div>
   )
 }
