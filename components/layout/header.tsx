8cc8f31b98a35f8e157a45bfb6547025c30cc890
codercarl1243 <codercarl1243@gmail.com>
2025-09-18 16:39:28 +1000

    UPDATE header should have links and they should communicate state of where the user is

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,21 @@
+'use client'
 import SkipLink from '@/components/skiplink'
 import clsx from 'clsx'
 import type { ComponentProps } from 'react'
+import Link from '@/components/link'
+import { usePathname } from 'next/navigation'
 
-export default function Header ({ className, ...props }: ComponentProps<'header'>) {
+export default function Header({ className, ...props }: ComponentProps<'header'>) {
+  const pathname = usePathname()
+
   return (
-        <header className={clsx('header', className)} aria-label="Main" {...props}>
-            <SkipLink />
-            <nav>
-                nav
-            </nav>
-        </header>
+    <header className={clsx('header', className)} aria-label="Main" {...props}>
+      <SkipLink />
+      <nav>
+        <Link href="/" className={clsx({ 'active': pathname === '/' })} aria-current={pathname === '/' ? 'true' : undefined}>Home</Link>
+        <Link href="/blog" className={clsx({ 'active': pathname.startsWith('/blog') })} aria-current={pathname === '/blog' ? 'true' : undefined}>Blog</Link>
+        <Link href="/about" className={clsx({ 'active': pathname === '/about' })} aria-current={pathname === '/about' ? 'true' : undefined}>About</Link>
+      </nav>
+    </header>
   )
 }
