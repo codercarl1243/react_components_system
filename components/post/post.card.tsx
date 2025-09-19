26fc945ad62c76c1eea4d18142355186f7c95754
codercarl1243 <codercarl1243@gmail.com>
2025-09-19 14:37:55 +1000

    UPDATE postcard should be more robust

--- components/post/post.card.tsx
+++ components/post/post.card.tsx
@@ -1,15 +1,36 @@
+'use client';
+
 import Image from 'next/image'
 import type { PostCardPropsType } from './post.type'
 import { useId } from 'react'
 import Link from '@/components/link'
 import Heading from '@/components/heading'
+import usePostCard from './usePostCard'
 
-export default function PostCard ({ variant = 'default', post, headingLevel = 3 }: PostCardPropsType) {
-  const { image, title } = post
-  const id = useId()
+export default function PostCard({ variant = 'default', post, headingLevel = 3 }: PostCardPropsType) {
+    const { image, title } = post
+    const id = useId()
+    const postCardSlug = `/blog/${post.slug}`
 
-  return (
-        <div data-variant={variant}>
+    const { handleContextMenu, handleClick, handleMouseDown, handleMouseUp } = usePostCard(postCardSlug)
+
+    return (
+        <article
+            data-variant={variant}
+            onClick={handleClick}
+            className="post-card"
+            onContextMenu={handleContextMenu}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+        >
+            <Heading className="post-card__title button"
+                data-styled="filled"
+                data-variant="accent"
+                headingLevel={headingLevel}
+                headingSize={4}>
+                <Link href={`/blog/${post.slug}`} aria-describedby={id}>{title}</Link>
+            </Heading>
+
             <Image
                 src={image.src}
                 alt={image.alt ?? ''}
@@ -17,12 +38,8 @@ export default function PostCard ({ variant = 'default', post, headingLevel = 3 
                 width={400}
                 sizes="(max-width: 768px) 100vw, 400px"
                 priority={variant === 'hero'}
+                className='post-card__image'
             />
-            <Heading className="post-card__title" headingLevel={headingLevel} >
-                {/* placeholder - change this when able. */}
-                <Link href="#" aria-describedby={id}>{title}</Link>
-            </Heading>
-            <span aria-hidden="true" id={id}>read more</span>
-        </div>
-  )
+        </article>
+    )
 }
