86b821291d00c300c60d9fb39e70764c24764449
codercarl1243 <codercarl1243@gmail.com>
2025-10-02 11:57:58 +1000

    UPDATE

--- components/utilities/spinner.tsx
+++ components/utilities/spinner.tsx
@@ -1,10 +1,9 @@
-import { RiLoader4Line } from "@remixicon/react";
-import Icon from "../icon";
-
-export default function Spinner(){
+export default function Spinner() {
     return (
-        <span className="spinner" aria-hidden="true">
-            <Icon icon={RiLoader4Line} color={"currentColor"}/>
-        </span>
+        <div className="spinner">
+            <div/>
+            <div/>
+            <div/>
+        </div>
     )
 }
