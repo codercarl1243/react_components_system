61ba23b3631b8b418b2b044de23df7935d183bd5
codercarl1243 <codercarl1243@gmail.com>
2025-10-21 19:26:30 +1000

    UPDATE button page for reference

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+'use client';
 import Button from '@/components/button'
 import Link from '@/components/link'
 import TabList from '@/components/tablist'
 
-export default function Home () {
+export default function Home() {
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>React Components</h1>
@@ -10,7 +11,7 @@ export default function Home () {
         <legend>Base Button</legend>
         <Button>click me</Button>
         <Button disabled>disabled button</Button>
-        <Button disabled isLoading></Button>
+        <Button isLoading></Button>
 
       </fieldset>
       <fieldset>
@@ -31,54 +32,57 @@ export default function Home () {
         <legend>Primary Button</legend>
         <Button data-style="outlined" data-variant="primary">outline button</Button>
         <Button data-style="filled" data-variant="primary">filled button</Button>
-        <Button data-style="filled" data-variant="primary" isLoading>filled button</Button>
+        <Button data-style="filled" data-variant="primary" isLoading>loading</Button>
       </fieldset>
       <fieldset>
         <legend>Secondary Button</legend>
         <Button data-style="outlined" data-variant="secondary">outline button</Button>
         <Button data-style="filled" data-variant="secondary">filled button</Button>
-        <Button data-style="filled" data-variant="secondary" isLoading>filled button</Button>
+        <Button data-style="filled" data-variant="secondary" isLoading>loading</Button>
       </fieldset>
-            <fieldset>
+      <fieldset style={{ display: "flex" }}>
         <legend>Accent Button</legend>
-        <Button data-style="outlined" data-variant="accent">outline button</Button>
+        <Button data-style="outlined" data-variant="accent">click</Button>
+        <Button data-style="outlined" data-variant="accent" isLoading>click</Button>
+        <Button data-style="outlined" data-variant="accent" disabled>click</Button>
+        {/*         
         <Button data-style="filled" data-variant="accent">filled button</Button>
-        <Button data-style="filled" data-variant="accent" isLoading>filled button</Button>
+        <Button data-style="filled" data-variant="accent" isLoading>loading</Button> */}
       </fieldset>
 
-           <TabList
-                defaultActiveTabId="panel1"
-                orientation="horizontal"
-                tabs={[
-                  {
-                    id: 'panel1',
-                    tabLabel: 'coding',
-                    panelContent: (
-                            <Link href="/categories/coding">
-                                show all of the posts to do with coding
-                            </Link>
-                    )
-                  },
-                  {
-                    id: 'panel2',
-                    tabLabel: 'accessibility',
-                    panelContent: (
-                            <Link href="/categories/accessibility">
-                                show all of the posts to do with accessibility
-                            </Link>
-                    )
-                  },
-                  {
-                    id: 'panel3',
-                    tabLabel: 'design-system',
-                    panelContent: (
-                            <Link href="/categories/design-systems">
-                                show all of the posts to do with design systems
-                            </Link>
-                    )
-                  }
-                ]}
-            />
+      <TabList
+        defaultActiveTabId="panel1"
+        orientation="horizontal"
+        tabs={[
+          {
+            id: 'panel1',
+            tabLabel: 'coding',
+            panelContent: (
+              <Link href="/categories/coding">
+                show all of the posts to do with coding
+              </Link>
+            )
+          },
+          {
+            id: 'panel2',
+            tabLabel: 'accessibility',
+            panelContent: (
+              <Link href="/categories/accessibility">
+                show all of the posts to do with accessibility
+              </Link>
+            )
+          },
+          {
+            id: 'panel3',
+            tabLabel: 'design-system',
+            panelContent: (
+              <Link href="/categories/design-systems">
+                show all of the posts to do with design systems
+              </Link>
+            )
+          }
+        ]}
+      />
     </>
   )
 }
