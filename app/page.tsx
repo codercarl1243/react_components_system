b19bc0ba9434a64a3f9b2387f21e471ef4ecd6e9
codercarl1243 <codercarl1243@gmail.com>
2025-10-22 08:59:00 +1000

    UPDATE page content for button examples should show useful variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,53 @@
 'use client';
 import Button from '@/components/button'
-import Link from '@/components/link'
-import TabList from '@/components/tablist'
+import Heading from '@/components/heading';
 
 export default function Home() {
   return (
     <>
-      <h1 style={{ textAlign: 'center' }}>React Components</h1>
+      <Heading headingLevel={1}>React Components</Heading>
       <fieldset>
         <legend>Base Button</legend>
         <Button>click me</Button>
-        <Button disabled>disabled button</Button>
-        <Button isLoading></Button>
+        <Button disabled>disabled</Button>
+        <Button isLoading>loading</Button>
 
       </fieldset>
       <fieldset>
         <legend>Filled Button</legend>
-        <Button data-style="filled">filled button</Button>
-        <Button data-style="filled" disabled>disabled button</Button>
-        <Button data-style="filled" isLoading>filled button</Button>
+        <Button data-style="filled">default</Button>
+        <Button data-style="filled" disabled>disabled</Button>
+        <Button data-style="filled" isLoading>filled</Button>
 
       </fieldset>
 
       <fieldset>
         <legend>outline Button</legend>
-        <Button data-style="outlined">outline button</Button>
-        <Button data-style="outlined" disabled>outline button</Button>
-        <Button data-style="outlined" isLoading>outline button</Button>
+        <Button data-style="outlined">default</Button>
+        <Button data-style="outlined" disabled>disabled</Button>
+        <Button data-style="outlined" isLoading>filled</Button>
       </fieldset>
       <fieldset>
         <legend>Primary Button</legend>
-        <Button data-style="outlined" data-variant="primary">outline button</Button>
-        <Button data-style="filled" data-variant="primary">filled button</Button>
-        <Button data-style="filled" data-variant="primary" isLoading>loading</Button>
+        <Button data-variant="primary">default</Button>
+        <Button data-style="outlined" data-variant="primary">outline</Button>
+        <Button data-style="filled" data-variant="primary">filled</Button>
+        <Button data-style="filled" data-variant="primary" isLoading>filled</Button>
       </fieldset>
       <fieldset>
         <legend>Secondary Button</legend>
-        <Button data-style="outlined" data-variant="secondary">outline button</Button>
-        <Button data-style="filled" data-variant="secondary">filled button</Button>
+        <Button data-variant="secondary">default</Button>
+        <Button data-style="outlined" data-variant="secondary">outline</Button>
+        <Button data-style="filled" data-variant="secondary">filled</Button>
         <Button data-style="filled" data-variant="secondary" isLoading>loading</Button>
       </fieldset>
       <fieldset style={{ display: "flex" }}>
         <legend>Accent Button</legend>
-        <Button data-style="outlined" data-variant="accent">click</Button>
-        <Button data-style="outlined" data-variant="accent" isLoading>click</Button>
-        <Button data-style="outlined" data-variant="accent" disabled>click</Button>
-        {/*         
-        <Button data-style="filled" data-variant="accent">filled button</Button>
-        <Button data-style="filled" data-variant="accent" isLoading>loading</Button> */}
+        <Button data-variant="accent">default</Button>
+        <Button data-style="outlined" data-variant="accent">outline</Button>
+        <Button data-style="filled" data-variant="accent">filled</Button>
+        <Button data-style="outlined" data-variant="accent" isLoading>loading</Button>
       </fieldset>
-
-      <TabList
-        defaultActiveTabId="panel1"
-        orientation="horizontal"
-        tabs={[
-          {
-            id: 'panel1',
-            tabLabel: 'coding',
-            panelContent: (
-              <Link href="/categories/coding">
-                show all of the posts to do with coding
-              </Link>
-            )
-          },
-          {
-            id: 'panel2',
-            tabLabel: 'accessibility',
-            panelContent: (
-              <Link href="/categories/accessibility">
-                show all of the posts to do with accessibility
-              </Link>
-            )
-          },
-          {
-            id: 'panel3',
-            tabLabel: 'design-system',
-            panelContent: (
-              <Link href="/categories/design-systems">
-                show all of the posts to do with design systems
-              </Link>
-            )
-          }
-        ]}
-      />
     </>
   )
 }
