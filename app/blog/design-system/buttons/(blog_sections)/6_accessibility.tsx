034b3c9791735334980bf6f6b7d14a7de43bcb9d
codercarl1243 <codercarl1243@gmail.com>
2025-10-30 18:17:55 +1000

    STYLING

--- app/blog/design-system/buttons/(blog_sections)/6_accessibility.tsx
+++ app/blog/design-system/buttons/(blog_sections)/6_accessibility.tsx
@@ -124,7 +124,7 @@ min-height: 44px;`} copyEnabled={false} />
                 <AnchorHeading headingLevel={4}>Assistive Technology isn&apos;t everything</AnchorHeading>
                 <p>Screen readers and other assistive tech is definitely powerful and does level the field to some degree, but they don't solve all problems:</p>
                 {/* add icons to these dot points */}
-                <List>
+                <List variant="circle" spacing="tight">
                     <li>Not all users with disabilities use screen readers</li>
                     <li>Visual feedback is critical for many users</li>
                     <li>Users benefit from predictable, stable UIs</li>
@@ -132,7 +132,7 @@ min-height: 44px;`} copyEnabled={false} />
 
                 <p>Our button addresses these concerns as much as possible, we ensure:</p>
                 {/* UPDATE for tick marks */}
-                <List>
+                <List variant="circle" spacing="tight">
                     <li>Semantic HTML and ARIA for screen readers</li>
                     <li>Visual state changes for sighted users</li>
                     <li>Stable layouts for cognitive accessibility</li>
@@ -144,18 +144,15 @@ min-height: 44px;`} copyEnabled={false} />
             <div className="flow-4">
                 <AnchorHeading headingLevel={4}>Further Reading</AnchorHeading>
                 <p>For a dive into accessible button patterns:</p>
-                <List>
+                <List variant="none" spacing="tight">
                     <li><Link href="https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/button_role">MDN: Button Accessibility</Link></li>
                     <li><Link href="https://www.w3.org/WAI/ARIA/apg/patterns/button/">W3C ARIA Authoring Practices: Button</Link></li>
-                    <li><Link href="https://adrianroselli.com/2021/01/multi-function-button.html">Adrian Roselli: Multi-Function Button</Link>
-                        <div>
-                            in depth article on creating a button with multiple states. This uses plain HTML, JS, & CSS.
-                        </div>
+                    <li><Link href="https://adrianroselli.com/2021/01/multi-function-button.html">Adrian Roselli: Multi-Function Button</Link> - an in-depth article on creating a button with multiple states. This uses plain HTML, JS, & CSS.
                     </li>
-                    <li><Link href="https://kittygiraudel.com/2024/03/29/on-disabled-and-aria-disabled-attributes/">Kitty Giraudel: a comparison of aria-disabled and disabled</Link> and how the use of either is not explicitly wrong</li>
+                    <li><Link href="https://kittygiraudel.com/2024/03/29/on-disabled-and-aria-disabled-attributes/">Kitty Giraudel: a comparison of aria-disabled and disabled</Link> - how the use of either is not explicitly wrong.</li>
                 </List>
                 <p className="bold">Design Systems: Buttons</p>
-                <List>
+                <List  variant="none" spacing="tight">
                     <li><Link href="https://m3.material.io/components/buttons/overview">Material Design</Link></li>
                     <li><Link href="https://developer.apple.com/design/human-interface-guidelines/buttons">Apple Human Interface Guidelines</Link></li>
                     <li><Link href="https://atlassian.design/components/button/examples">Atlassian</Link></li>
