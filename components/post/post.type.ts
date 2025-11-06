199b555235300561e5388d7d96b3e2f96b16b24d
codercarl1243 <codercarl1243@gmail.com>
2025-11-07 09:37:53 +1000

    UPDATE types should be more explicit for posts

--- components/post/post.type.ts
+++ components/post/post.type.ts
@@ -1,23 +1,42 @@
-import type { ComponentProps, ReactNode } from 'react'
-import type { HeadingLevelsType } from '../heading/heading.type';
+import type { ComponentProps } from 'react'
+import type { HeadingLevelsType } from '@/components/heading/heading.type';
+import type { TImage } from '@/components/image/image.type';
+import type { Branded } from '@/types/utility/brand';
 
 export type PostImageObjType = {
-    src: string;
-    alt?: string;
+    src: TImage['src'];
+    alt: TImage['alt'];
 };
 
+export type PostId = Branded<string, 'PostId'>;
+
 export type PostType = {
+    id: PostId;
     title: string;
     subtitle?: string;
     image: PostImageObjType;
-    children: ReactNode;
+    excerpt: string;
+    href: string;
+    relatedPostIds: PostId[];
+    lastModified: Date;
+    published: Boolean;
+    featured?: Boolean;
 };
 
-export type PostBannerPropsType = Pick<PostType, 'title' | 'image' | 'subtitle'> & {headingId?: string};
+export type PostSummary = {
+    id: PostId;
+    href: PostType['href'];
+    title: PostType['title'];
+    image: PostType['image'];
+    excerpt?: PostType['excerpt'];
+    lastModified: PostType['lastModified'];
+};
+
+export type PostBannerPropsType = Pick<PostType, 'title' | 'image' | 'subtitle'> & { headingId?: string };
 
 export type PostCardPropsType = {
     variant?: 'featured' | 'card';
-    post: Pick<PostType, 'title' | 'image'> & {slug: string};
+    post: PostSummary;
     headingLevel: Exclude<HeadingLevelsType, 1>;
 };
 
