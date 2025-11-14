c6053d9ce79cd474d84c62846b6e9cf2d1106cd9
codercarl1243 <codercarl1243@gmail.com>
2025-11-14 16:54:54 +1000

    TYPO

--- components/hamburger/context.tsx
+++ components/hamburger/context.tsx
@@ -6,7 +6,7 @@ export const HamburgerContext = createContext<HamburgerState | null>(null);
 export function useHamburgerContext(): HamburgerState {
   const ctx = useContext(HamburgerContext);
   if (!ctx) {
-    throw new Error("Hamburger components must be used within <Hamburger.Root>");
+    throw new Error("Hamburger components must be used within <Hamburger.Wrapper>");
   }
   return ctx;
 }
