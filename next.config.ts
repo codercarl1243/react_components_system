5b3643a82bd2d32e3722fad654b66f9d90e14bc8
codercarl1243 <codercarl1243@gmail.com>
2025-12-08 07:59:22 +1000

    UPDATE image variant qualities and add qualities to next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    qualities: [75, 100],
+    // remotePatterns: [
+    //   {host: '', protocol: "https"}
+    // ]
+  }
   /* config options here */
 }
 
