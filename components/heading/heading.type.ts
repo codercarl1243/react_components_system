a0f7ea7a05d1bc43cf9e57c344d6ac5275b4922a
codercarl1243 <codercarl1243@gmail.com>
2025-12-08 12:00:53 +1000

    UPDATE heading type to use new BlockWrapper

--- components/heading/heading.type.ts
+++ components/heading/heading.type.ts
@@ -1,16 +1,15 @@
 import type { IconProps } from '@/components/icon/icon.type';
-import type { InlineProps } from '@/components/primitives/types';
+import type { BlockWrapperProps } from '@/components/primitives/types';
 
 
 export type ValidHeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type HeadingVariant = 'primary' | 'secondary' | 'accent' | 'neutral';
 
 export type BaseHeadingProps = {
-    as?: ValidHeadingTag;
     headingSize?: 1 | 2 | 3 | 4 | 5 | 6;
     icon?: IconProps['icon'];
     variant?: HeadingVariant;
 };
 
-export type HeadingPropsType<T extends ValidHeadingTag = "h3"> = Omit<InlineProps<T>, 'variant' | 'as'> & BaseHeadingProps;
-export type AnchorHeadingPropsType<T extends ValidHeadingTag = "h3"> =  Omit<InlineProps<T>, 'variant' | 'as'> & BaseHeadingProps & { prefix?: string; };
+export type HeadingPropsType<T extends ValidHeadingTag = "h3"> = BlockWrapperProps<T, BaseHeadingProps>;
+export type AnchorHeadingPropsType<T extends ValidHeadingTag = "h3"> = BlockWrapperProps<T, BaseHeadingProps & { prefix?: string; }>;
