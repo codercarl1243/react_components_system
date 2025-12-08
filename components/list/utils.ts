7b258e95cbfe7e283e24a060cf71514fdf4f52f1
codercarl1243 <codercarl1243@gmail.com>
2025-12-08 12:00:13 +1000

    UPDATE list component should be abstracted for better maintenance and clearer code

---
+++ components/list/utils.ts
@@ -0,0 +1,33 @@
+import type { OlMarker, UlMarker, ValidListTag, ValidSpacing } from "./types";
+
+export const ulMarkerClasses: Record<UlMarker, string> = {
+    default: 'list--circle',
+    circle: 'list--circle',
+    disc: 'list--disc',
+    square: 'list--square',
+    none: 'list--none'
+};
+
+export const olMarkerClasses: Record<OlMarker, string> = {
+    default: 'list--decimal',
+    decimal: 'list--decimal',
+    'lower-alpha': 'list--lower-alpha',
+    'upper-alpha': 'list--upper-alpha',
+    'lower-roman': 'list--lower-roman',
+    'upper-roman': 'list--upper-roman',
+    none: 'list--none'
+};
+
+export const getMarkerClass = (listType: ValidListTag, marker: OlMarker | UlMarker) => {
+    return listType === "ol"
+        ? olMarkerClasses[marker as OlMarker]
+        : ulMarkerClasses[marker  as UlMarker];
+}
+
+export const getSpacingClass = (spacing: ValidSpacing) => {
+    return {
+        tight: 'list--tight',
+        normal: 'list--normal',
+        loose: 'list--loose'
+    }[spacing]
+}
