69e6efb83da235b143c323e87bc84d94317069e2
codercarl1243 <codercarl1243@gmail.com>
2025-12-12 11:41:05 +1000

    UPDATE ensure that id is required when label exists on select element

--- components/select/types.ts
+++ components/select/types.ts
@@ -1,17 +1,19 @@
 import type { BlockProps } from "@/components/primitives/types";
 import type { ReactNode } from "react";
 
+export type LabelProps = { label: ReactNode; id: string } & Omit<BlockProps<"label">, "as" | "htmlFor" | "children">;
+
 export type SelectOptionProps = BlockProps<'option'> & {
     label: ReactNode;
     value: string;
 };
 
-export type LabelProps = { label: ReactNode; id: string } & Omit<BlockProps<"label">, "as" | "htmlFor" | "children">;
+type SelectBaseProps = BlockProps<"select"> & {
+    options: SelectOptionProps[];
+    error?: ReactNode;
+    helperText?: ReactNode;
+};
 
 export type SelectProps =
-    BlockProps<"select"> & {
-        label?: ReactNode;
-        options: SelectOptionProps[];
-        error?: ReactNode;
-        helperText?: ReactNode;
-    };
+    | (SelectBaseProps & { label?: undefined })
+    | (SelectBaseProps & { label: ReactNode; id: string });
