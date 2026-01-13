982f90c1dcf3f89d5afe853f5bd3c98eed4ce777
codercarl1243 <codercarl1243@gmail.com>
2026-01-13 15:10:55 +1000

    UPDATE filepath and folder name now that content is confirmed

--- app/blog/design-system/theming/page.tsx
+++ app/blog/design-system/theming/page.tsx
@@ -1,17 +1,17 @@
 import Post from "@/components/post";
-import Introduction from "./(blog_sections_new)/1_introduction";
-import WhatWeAreBuilding from "./(blog_sections_new)/2_whatWeAreBuilding";
-import Why from './(blog_sections_new)/3_why';
-import Tokens from './(blog_sections_new)/4_tokens';
-import Structure from './(blog_sections_new)/5_structure';
-import Variants from './(blog_sections_new)/6_variants';
-import Appearance from './(blog_sections_new)/7_appearance';
-import Paint from './(blog_sections_new)/8_paint';
-import PuttingItAllTogether from './(blog_sections_new)/9_puttingItAllTogether';
-import Summary from './(blog_sections_new)/10_summary';
-import Resources from './(blog_sections_new)/resources';
+import Introduction from "./(blog_sections)/1_introduction";
+import WhatWeAreBuilding from "./(blog_sections)/2_whatWeAreBuilding";
+import Why from './(blog_sections)/3_why';
+import Tokens from './(blog_sections)/4_tokens';
+import Structure from './(blog_sections)/5_structure';
+import Variants from './(blog_sections)/6_variants';
+import Appearance from './(blog_sections)/7_appearance';
+import Paint from './(blog_sections)/8_paint';
+import PuttingItAllTogether from './(blog_sections)/9_puttingItAllTogether';
+import Summary from './(blog_sections)/10_summary';
+import Resources from './(blog_sections)/resources';
 import PostBanner from "@/components/post/post.banner";
-import Sidebar from "./(blog_sections_new)/sidebar";
+import Sidebar from "./(blog_sections)/sidebar";
 
 export default function ThemingPage() {
 
