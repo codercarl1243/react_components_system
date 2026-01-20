d8a7b3747d67358c63fbd1703cd91dfe0220e001
codercarl1243 <codercarl1243@gmail.com>
2026-01-20 13:49:02 +1000

    CLEANUP

--- components/post/post.type.ts
+++ components/post/post.type.ts
@@ -1,18 +1,8 @@
-import type { ValidHeadingTag } from '@/components/heading/heading.type';
-import type { PostSummary, PostType } from '@/lib/blog/blog.types';
+import type { PostSummary } from '@/lib/blog/blog.types';
 import type { BlockProps, StackProps } from '@/components/primitives/types';
 import { ComponentProps } from 'react';
 import { Variant } from '@/types/variant';
 
-export type PostBannerContent = Pick<PostType, 'title' | 'subtitle' | 'image'>;
-
-export type PostBannerProps = {
-  post?: PostBannerContent;
-  title?: string;
-  subtitle?: string;
-  headingId: string;
-  image?: PostType['image'];
-}
 
 export type PostCardPropsType = {
   post: PostSummary;
@@ -21,7 +11,6 @@ export type PostCardPropsType = {
 export type SectionProps = StackProps<'section'> & {
   width?: "content" | "bleed" | "full"
 };
-
 
 export type PostPropsType = BlockProps<'article'>;
 
