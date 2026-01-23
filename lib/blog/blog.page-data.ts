ebcaa7cfba34067541954afbe798fa7167a5ea21
codercarl1243 <codercarl1243@gmail.com>
2026-01-23 12:00:45 +1000

    UPDATE add explicit return typing

--- lib/blog/blog.page-data.ts
+++ lib/blog/blog.page-data.ts
@@ -1,9 +1,14 @@
 import { notFound } from "next/navigation";
 import { getBlogPostById, getRelatedPosts } from "@/lib/blog/blog.data";
 import { getAuthorById } from "@/lib/blog/authors/authors.data";
-import type { PostId } from "@/lib/blog/blog.types";
+import type { PostId, PostSummary, PostType } from "@/lib/blog/blog.types";
+import type { Author } from "@/lib/blog/authors/authors.types";
 
-export default function getBlogPostPageData(id: PostId | string) {
+export default function getBlogPostPageData(id: PostId | string): {
+  post: PostType;
+  relatedPosts: PostSummary[];
+  author: Author | undefined;
+} {
   const post = getBlogPostById(id);
 
   if (!post) {
