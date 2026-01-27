f6e22ebdec50b9602b4727ff63f13fe3ed945baf
codercarl1243 <codercarl1243@gmail.com>
2026-01-27 14:43:56 +1000

    UPDATE bring buttons in line with global types

--- components/button/button.type.ts
+++ components/button/button.type.ts
@@ -2,21 +2,19 @@ import type { MouseEvent } from 'react'
 import type { IconProps } from '@/components/icon/icon.type';
 import type { BlockWrapperProps } from '../primitives/types';
 import type { AccessibleLabel } from '@/types/accessibility';
+import { Variant, VariantAppearance } from '@/types/variant';
 
 export type MouseEventType = MouseEvent<HTMLButtonElement>;
 
 export type ButtonClickHandler = (event: MouseEventType) => void | Promise<void>;
-
-export type TButtonAppearance = 'outlined' | 'filled' | 'ghost';
-export type TButtonVariant = 'primary' | 'secondary' | 'accent' | 'neutral' | 'danger' | 'warning' | 'inverse';
 
 type BaseButtonProps = {
   disabled?: boolean;
   isLoading?: boolean;
   icon?: IconProps['icon'];
   onClick?: ButtonClickHandler;
-  variant?: TButtonVariant;
-  appearance?: TButtonAppearance;
+  variant?: Variant;
+  appearance?: VariantAppearance;
 };
 
 export type ButtonProps = Omit<BlockWrapperProps<"button", BaseButtonProps>, 'as'>
