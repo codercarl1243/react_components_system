8c9f7a751adc9926d63889aca8fc5931ab46018a
codercarl1243 <codercarl1243@gmail.com>
2026-02-05 14:52:54 +1000

    UPDATE add extra spacing for homepage grid. use grid from posts to keep consistent layouts

--- components/homepage/post.latest.tsx
+++ components/homepage/post.latest.tsx
@@ -12,17 +12,19 @@ export default function LatestPosts() {
       className="homepage--latest-posts width-bleed"
     >
       <Heading as="h2" className="center mb-4" headingSize={4}>Latest posts</Heading>
-      <List as="ul" marker="none" className="latest-posts__grid">
-        {featuredPost && <li className="latest-post latest-posts__featured">
-          <PostCard post={featuredPost} layout="large"/>
-        </li>}
+      <div className="post-card__grid-container">
+        <List as="ul" marker="none" className="post-card__grid p-0 flow-6">
+          {featuredPost && <li className="latest-post latest-posts__featured" data-layout="large">
+            <PostCard post={featuredPost} layout="large" />
+          </li>}
 
-        {posts.map(post => (
-          <li key={post.id} className="latest-post latest-posts__item">
-            <PostCard post={post} />
-          </li>
-        ))}
-      </List>
+          {posts.map(post => (
+            <li key={post.id} className="latest-post latest-posts__item">
+              <PostCard post={post} layout="default" />
+            </li>
+          ))}
+        </List>
+      </div>
     </Block>
   )
 }
