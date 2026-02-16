31d40809ca40709797acba5970fab3a3a2de4283
codercarl1243 <codercarl1243@gmail.com>
2026-02-16 12:53:10 +1000

    ADD simple styling to add texture to bg

--- components/homepage/hero.tsx
+++ components/homepage/hero.tsx
@@ -1,16 +1,19 @@
 import BubbleButton from '@/components/button/bubbleButton'
 import Heading from '@/components/heading'
+import Octagons from '@/components/octagons'
 
 export default function Hero() {
 
   return (
-    <div className='width-full'>
+    <div className='width-full hero relative'>
+      <Octagons />
       <Heading as={"h1"} id="hero-heading" className='mb-4' variant='neutral' variantAppearance='filled'>
         Hey, I'm Carl
       </Heading>
       <p className='mb-16'>
         On this site I share my <span className="fun-underline">process</span>, <span className="fun-underline">tools</span>, <span className="fun-underline">lessons</span>, and the occasional bit of <span className="fun-underline">creative chaos</span> from working with the web.
       </p>
+
       <BubbleButton />
     </div>
   )
