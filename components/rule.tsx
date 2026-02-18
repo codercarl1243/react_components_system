0d4ee9678eb10a4629b2a7a83e278e5520f5a9b3
codercarl1243 <codercarl1243@gmail.com>
2026-02-19 08:49:07 +1000

    UPDATE move rule heading class into callout heading so it is reusable

--- components/rule.tsx
+++ components/rule.tsx
@@ -6,7 +6,7 @@ export default function Rule({ children }: { children: ReactNode }) {
 
     return (
         <aside className="rule">
-            <Row as="strong" className="rule__heading font-accent">System rule</Row>
+            <Row as="strong" className="callout__heading font-accent">System rule</Row>
             <p className="rule__content">
                 {children}
             </p>
