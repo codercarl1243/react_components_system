ab7b78793a1b2e3a251330582fbef955e396c42d
codercarl1243 <codercarl1243@gmail.com>
2026-02-23 16:08:15 +1000

    UPDATE remove icon from props

--- components/button/switch/type.ts
+++ components/button/switch/type.ts
@@ -1,11 +1,9 @@
-import type { IconProps } from "@/components/icon/icon.type";
 import type { Paint } from "@/types/paint";
 import type { Variant, VariantAppearance } from "@/types/variant";
 import type { ComponentProps } from "react";
 
 export type TSwitchProps = {
     checked: boolean;
-    icon?: Element | IconProps['icon'];
     variant?: Variant;
     variantAppearance?: VariantAppearance;
     paint?: Paint;
