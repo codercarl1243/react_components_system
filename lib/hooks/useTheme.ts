2e1127307e50cbf61095acd61c137ac8a0762784
codercarl1243 <codercarl1243@gmail.com>
2026-02-26 12:07:31 +1000

    LINTING

--- lib/hooks/useTheme.ts
+++ lib/hooks/useTheme.ts
@@ -7,10 +7,7 @@ export function useTheme(initialTheme: string) {
     const [theme, setTheme] = useState<string>(initialTheme);
 
     useEffect(() => {
-        console.log("use effect triggred")
-        console.log("document.documentElement", document.documentElement)
         document.documentElement.dataset.theme = theme;
-        console.log("THEMEEEE", theme)
         document.cookie = `theme=${theme}; path=/; max-age=31536000`;
     }, [theme]);
 
