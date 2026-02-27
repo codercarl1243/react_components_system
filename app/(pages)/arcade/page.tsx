2563bbdb4107107f9a6e384e37b41dc1d19c0c4f
codercarl1243 <codercarl1243@gmail.com>
2026-02-27 10:31:47 +1000

    UPDATE use internal link component over next link

--- app/(pages)/arcade/page.tsx
+++ app/(pages)/arcade/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "@/components/link";
 import { gamesRegistry } from "./gamesRegistry";
 import List from "@/components/list";
 import { Stack } from "@/components/primitives";
