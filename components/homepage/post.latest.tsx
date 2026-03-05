8b785a5df7c904f9c1df661a5283a5d92e252d2e
codercarl1243 <codercarl1243@gmail.com>
2026-03-05 17:29:15 +1000

    update content grid name from  card grid

--- components/homepage/post.latest.tsx
+++ components/homepage/post.latest.tsx
@@ -13,18 +13,24 @@ export default function LatestPosts() {
       className="homepage__latest-posts homepage__section width-full"
     >
       <Heading as="h2" className="homepage__latest-posts__heading center mb-16" headingSize={2}>Latest posts</Heading>
-      <div className="homepage__latest-posts__posts post-card__grid-container">
-        <List as="ul" marker="none" className="post-card__grid p-0 flow-6">
-          {featuredPost && <li key={featuredPost.id} className="latest-post latest-posts__featured" data-layout="large">
-            <PostCard post={featuredPost} layout="large" />
-          </li>}
 
+      <div className="homepage__latest-posts__posts content-grid__container">
+
+        <List as="ul" marker="none" className="content-grid p-0 flow-6">
+          {featuredPost && (
+            <li key={featuredPost.id} className="content-grid__item latest-post latest-posts__featured" data-layout="large">
+              <PostCard post={featuredPost} layout="large" />
+            </li>
+          )}
+
+          {/* {[...posts,...posts,...posts].map(post => ( */}
           {posts.map(post => (
-            <li key={post.id} className="latest-post latest-posts__item">
+            <li key={post.id} className="content-grid__item latest-post latest-posts__item">
               <PostCard post={post} layout="default" />
             </li>
           ))}
         </List>
+
       </div>
     </Block>
   )
