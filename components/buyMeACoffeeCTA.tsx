3df22ebfd9e6c82da21a27d58c2564c2c425d407
codercarl1243 <codercarl1243@gmail.com>
2026-03-06 17:31:22 +1000

    UPDATE footer should include links to socials and cta styling should be broader

--- components/buyMeACoffeeCTA.tsx
+++ components/buyMeACoffeeCTA.tsx
@@ -1,17 +1,19 @@
-import { RiJavaFill } from "@remixicon/react";
+import { RiHandHeartLine, RiJavaFill } from "@remixicon/react";
 import Link from "@/components/link";
+import type { LinkProps } from "@/components/link/link.type";
 import Icon from "@/components/icon";
 import clsx from "clsx";
 
-export default function BuyMeACoffeeCTA({ className }: { className?: string }) {
+export default function BuyMeACoffeeCTA({ className, ...props }: LinkProps ) {
 
     return (
         <Link
-            className={clsx("buyMeACoffee__Link w-fit px-8 py-2", className)}
+            className={clsx("buyMeACoffee__Link contact-link--external contact-link", className)}
             href="https://buymeacoffee.com/codercarl"
             rel="noopener noreferrer"
+            {...props}
         >
-            <Icon icon={RiJavaFill} />
+            <Icon className="contact-link__icon" icon={RiHandHeartLine} />
             <span>Buy me a coffee</span>
         </Link>
     )
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,13 +2,36 @@ import clsx from 'clsx'
 import type { ComponentProps } from 'react'
 import Link from '@/components/link'
 import { Inline } from '@/components/primitives'
+import BuyMeACoffeeCTA from '../buyMeACoffeeCTA'
+import { RiMailSendLine, RiGithubFill, RiLinkedinLine } from '@remixicon/react'
+import Icon from '@/components/icon'
 
 // TODO: add links to linkedin and github
-export default function Footer ({ className, ...props }: ComponentProps<'footer'>) {
+export default function Footer({ className, ...props }: ComponentProps<'footer'>) {
   const year = new Date().getFullYear()
   return (
-        <Inline as="footer" align="center" className={clsx('footer', className)} {...props}>
-            <p>&copy; {year} Carl Davidson. All rights reserved. <span>|</span> <Link href="mailto:codercarl1243@gmail.com">codercarl1243@gmail.com</Link></p>
-        </Inline>
+    <Inline as="footer" align="center" justify='even' className={clsx('footer', className)} {...props}>
+
+      <p>&copy; {year} Carl Davidson. All rights reserved.</p>
+      <span>|</span>
+      <ul className='footer__socials'>
+        <li><BuyMeACoffeeCTA className="contact-link"/></li>
+        <li>
+          <Link className="contact-link" href="mailto:codercarl1243@gmail.com" showExternalIcon={false}>
+            <Icon className="contact-link__icon" icon={RiMailSendLine} /><span>Email</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="contact-link" href="https://github.com/codercarl1243" showExternalIcon={false}>
+            <Icon className="contact-link__icon" icon={RiGithubFill} /><span>Github</span>
+          </Link>
+        </li>
+        <li>
+          <Link className="contact-link" href="https://www.linkedin.com/in/carl-davidson/" showExternalIcon={false}>
+            <Icon className="contact-link__icon" icon={RiLinkedinLine} /><span>LinkedIn</span>
+          </Link>
+        </li>
+      </ul>
+    </Inline>
   )
 }
