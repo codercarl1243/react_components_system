4d27dfb4c9b27de9b05265aafd0857675532c75b
codercarl1243 <codercarl1243@gmail.com>
2026-03-13 12:17:23 +1000

    UPDATE use consistent quote style

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,21 +1,21 @@
-'use client'
-import clsx from 'clsx'
-import { type ComponentProps } from 'react'
-import SkipLink from '@/components/skiplink'
-import { Hamburger } from '@/components/hamburger'
-import { RiMenuLine } from '@remixicon/react'
-import NavLinks from '@/components/navLinks'
-import DarkModeSwitch from '../darkModeSwitch'
-import BubbleButton from '../bubbles/bubbleButton'
+"use client"
+import clsx from "clsx"
+import { type ComponentProps } from "react"
+import SkipLink from "@/components/skiplink"
+import { Hamburger } from "@/components/hamburger"
+import { RiMenuLine } from "@remixicon/react"
+import NavLinks from "@/components/navLinks"
+import DarkModeSwitch from "../darkModeSwitch"
+import BubbleButton from "../bubbles/bubbleButton"
 
-export default function Header({ className, userTheme, ...props }: ComponentProps<'header'> & { userTheme: string }) {
+export default function Header({ className, userTheme, ...props }: ComponentProps<"header"> & { userTheme: string }) {
 
   return (
     <>
       <Hamburger.Wrapper
         as="header"
         horizontal="left"
-        vertical='top'
+        vertical="top"
         className={clsx("header--wrapper", className)}
         breakpoint="mobile"
         menuId="Primary-Nav"
@@ -23,8 +23,8 @@ export default function Header({ className, userTheme, ...props }: ComponentProp
       >
         <SkipLink />
         <Hamburger.Toggle
-          variantAppearance='filled'
-          variant='primary'
+          variantAppearance="filled"
+          variant="primary"
           openIcon={RiMenuLine}
         >
           Menu
@@ -32,12 +32,12 @@ export default function Header({ className, userTheme, ...props }: ComponentProp
         <Hamburger.Menu
           as="div"
         >
-          <div className='nav__wrapper'>
+          <div className="nav__wrapper">
             <NavLinks
-              className='nav__primary'
+              className="nav__primary"
               aria-label="Primary"
             />
-            <div className='header__toggles'>
+            <div className="header__toggles">
               <BubbleButton />
               <DarkModeSwitch userTheme={userTheme} />
             </div>
