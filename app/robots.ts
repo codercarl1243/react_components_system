1be7e26405823a23a3f98d6bb09a3eff84b7c37d
codercarl1243 <codercarl1243@gmail.com>
2026-03-19 18:55:04 +1000

    UPDATE use robots .ts with nextjs norms

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/admin',
+    },
+    sitemap: 'https://codercarl.dev/sitemap.xml',
+  }
+}
